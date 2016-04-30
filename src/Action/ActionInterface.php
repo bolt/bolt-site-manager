4573d571c62a606f3f12d190b3cd7a9e9e06f2da
Gawain Lynch <gawain.lynch@gmail.com>
2016-04-30 16:19:06 +0200

    Interface for action classes

---
+++ src/Action/ActionInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Bolt\Deploy\Action;
+
+/**
+ * Action Interface.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+interface ActionInterface
+{
+    /**
+     * Execute the action.
+     */
+    public function execute();
+}
