66b68f3fdc11d591b722b423016a7ee164fe6720
Gawain Lynch <gawain.lynch@gmail.com>
2016-05-03 13:29:43 +0200

    Remove bash scripts

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-WD=$(pwd)
-
-ln -s bolt-create      /usr/bin/bolt-create
-ln -s bolt-permissions /usr/bin/bolt-permissions
-ln -s bolt-setup       /usr/bin/bolt-setup
-ln -s bolt-update      /usr/bin/bolt-update
