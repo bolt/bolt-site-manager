e0c36e4fc9e4056e8098e16457a7bdcdf06a14a9
Gawain Lynch <gawain.lynch@gmail.com>
2017-01-23 16:01:45 +0100

    Bump version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -13,7 +13,7 @@ use Symfony\Component\Console\Application as BaseApplication;
  */
 class Application extends BaseApplication
 {
-    const VERSION = '1.0.0';
+    const VERSION = '1.1.0';
 
     public static $timestamp;
 
